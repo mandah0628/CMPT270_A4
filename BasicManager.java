69effa22bd23f0657d31d02107d77584f25db64f
Matt Tsogt <mandah0628@gmail.com>
2023-10-21 22:07:33 -0600

    lmao

--- BasicManager.java
+++ BasicManager.java
@@ -3,11 +3,7 @@
   Copyright (c) 2021
   All rights reserved.
 
-  This document contains resources for homework assigned to students of
-  of CMPT 270 and shall not be distributed without permission.  Posting this
-  file to a public or private website, or providing this file to any person
-  not registered in CMPT 270 constitutes Academic Misconduct according to
-  to the University of Saskatchewan Policy on Academic Misconduct.
+ 
 
   Synopsis:
      Starter file for Assignment 4
